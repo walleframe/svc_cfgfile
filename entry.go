671f194538b25148089a2cbcede7b93d8e7a7489
czy <czy463@163.com>
2023-12-18 16:16:25 +0800

    config file load

---
+++ entry.go
@@ -0,0 +1,29 @@
+package svc_cfgfile
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/spf13/pflag"
+	"github.com/walleframe/walle/services/configcentra"
+)
+
+var appName string
+var dumpConfigFlag bool
+
+// UseConfigFile use config file to start app
+func UseConfigFile() {
+	configcentra.ConfigCentraBackend = &ConfigFileService{}
+
+	appName = filepath.Base(os.Args[0])
+	if runtime.GOOS == "windows" {
+		appName = strings.TrimSuffix(appName, ".exe")
+	}
+
+	// 命令行参数绑定
+	pflag.BoolVar(&dumpConfigFlag, "dump_config", false, "dump service config")
+	pflag.String("config_file", fmt.Sprintf("./conf/%s.toml", appName), "app config file")
+}
